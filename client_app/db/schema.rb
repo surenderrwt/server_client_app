51626412ef89f4855c7902a1674192d4216e1a42
surender rawat <rawat.surender87@gmail.com>
2020-01-28 02:06:38 +0530

    Remove User model and its associated files

--- client_app/db/schema.rb
+++ client_app/db/schema.rb
@@ -15,9 +15,4 @@ ActiveRecord::Schema.define(version: 2020_01_22_190449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
